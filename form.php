784513a346dbec0085c2adf4b7ea9cac63efc71f
Victor de Sousa Lemos <58195944+Victorsousalemos@users.noreply.github.com>
2022-10-26 09:44:42 -0300

    Update form.php

--- form.php
+++ form.php
@@ -5,9 +5,8 @@
                     $input = stripslashes($input);
                     $input = htmlspecialchars($input);
 
-                    return $input;
-
-                    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                   
+                    ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         $nome = $_POST['nome'];
                         $email = $_POST['email'];                        
                         $telefone = $_POST['telefone'];
